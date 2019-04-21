303d72eef92c5c7db75c70944e530b7210b05087
Alejandro <alejandrehl@icloud.com>
2019-04-21 02:22:00 -0400

    Guardando avances

--- web/app.py
+++ web/app.py
@@ -2,6 +2,8 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
 from bson.json_util import dumps
+import pokebase as pb
+import requests as req
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,11 +14,29 @@ leagues = db["leagues"]
 trainers = db["trainers"]
 pokemons = db["pokemons"]
 
+def getPokemons():
+    url = "https://pokeapi.co/api/v2/pokemon"
+    data = req.get(url).json()
+    return jsonify(data["results"])
+
 def leagueExist(league_name):
     if leagues.find({"league_name": league_name}).count() == 0:
         return False
     else:
         return True
+
+def generateReturnDictionary(status, msg):
+    retJson = {
+        "status" : status,
+        "msg" : msg
+    }
+    return retJson
+
+def verifyLeagueName(league_name):
+    if not leagueExist(league_name):
+        return generateReturnDictionary(301, "Invalid league name."), True
+
+    return None, False
 
 class League(Resource):
     def post(self):
@@ -41,19 +61,6 @@ class League(Resource):
 
         return jsonify(retJson)
 
-def generateReturnDictionary(status, msg):
-    retJson = {
-        "status" : status,
-        "msg" : msg
-    }
-    return retJson
-
-def verifyLeagueName(league_name):
-    if not leagueExist(league_name):
-        return generateReturnDictionary(301, "Invalid league name."), True
-
-    return None, False
-
 class Trainer(Resource):
     def post(self):
         postedData = request.get_json()
@@ -68,22 +75,22 @@ class Trainer(Resource):
         if pokemons_number < 1 or pokemons_number > 6:
             return jsonify(generateReturnDictionary(301, "Invalid pokemons number."))
         
-        if trainers.find({"trainer_name": trainer_name}).count() == 0:
+        if not trainers.find({"trainer_name": trainer_name}).count() == 0:
             return jsonify(generateReturnDictionary(301, "Trainer is already in a league."))
 
-        trainers.insert({
-            "league_name" : league_name,
-            "trainer_name" : trainer_name,
-            "pokemons_number" : pokemons_number
-        })
-
-        count = 0
-        for count in range(pokemons_number):
-            count += 1
-            pokemons.insert({
-                "pokemon_name" : "pokemon "+str(count),
-                "trainer_name" : trainer_name
-            })
+        # trainers.insert({
+        #     "league_name" : league_name,
+        #     "trainer_name" : trainer_name,
+        #     "pokemons_number" : pokemons_number
+        # })
+        return getPokemons()
+        # count = 0
+        # for count in range(pokemons_number):
+        #     count += 1
+        #     pokemons.insert({
+        #         "pokemon_name" : "pokemon "+str(count),
+        #         "trainer_name" : trainer_name
+        #     })
 
         retJson = {
             "status" : 200,
@@ -93,18 +100,6 @@ class Trainer(Resource):
 
 api.add_resource(League, '/leagues')
 api.add_resource(Trainer, '/trainers')
-
-@app.route("/allleagues")
-def getLeagues():
-    return dumps(leagues.find())
-    
-@app.route("/allpokemons")
-def getPokemons():
-    return dumps(pokemons.find())
-
-@app.route("/alltrainers")
-def getTrainers():
-    return dumps(trainers.find())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
